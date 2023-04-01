5d429b3f800cf0a4154856085efcbab05461a25e
sulthanakeysie <sulthanakeysi@gmail.com>
2023-04-01 13:26:36 +0530

    yup validation for login and signup page

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,81 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
 import { useNavigate } from "react-router";
 import { login } from "../../services/AuthService";
 import { toast } from "react-toastify";
+import * as Yup from "yup";
 import "./Auth.css";
+
+const validate = Yup.object({
+  email: Yup.string().email("Invalid email address").required("Required"),
+  password: Yup.string().required("Required"),
+});
 
 export default function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleEmailError = () => {
-    if (!email) setEmailError("Email is required");
-    else setEmailError("");
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handlePasswordError = () => {
-    if (!password) {
-      setPasswordError("Password is required");
-    } else if (password.length < 6) {
-      setPasswordError("Password must be at least 6 characters");
-    } else {
-      setPasswordError("");
-    }
-  };
-
-  const Login = (event) => {
-    event.preventDefault();
-
-    handleEmailError();
-    handlePasswordError();
-
-    if (!emailError && !passwordError) {
-      login({ email, password })
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema:validate,
+    onSubmit: (values) => {
+      login({ email: values.email, password: values.password })
         .then((res) => {
-          console.log(res);
           localStorage.setItem("api_key", res.data.user.api_key);
           navigate("/images");
         })
         .catch((err) => {
-          toast.error(err.message);
+          toast.error(err.response.data.message);
         });
-    }
-  };
+    },
+  });
+
   return (
     <div className="form-page-wrapper">
-      <form className="auth-form-wrapper" onSubmit={Login}>
+      <form className="auth-form-wrapper" onSubmit={formik.handleSubmit}>
         <h2>Login</h2>
         <div className="form-group-wrapper">
           <label htmlFor="email">Email:</label>
           <input
             id="email"
-            value={email}
-            onChange={handleEmailChange}
-            onBlur={handleEmailError}
+            name="email"
+            type="text"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
-          {emailError && <span className="error">{emailError}</span>}
+          {formik.touched.email && formik.errors.email && (
+            <span className="error">{formik.errors.email}</span>
+          )}
         </div>
-        <div className="form-page-wrapper">
+        <div className="form-group-wrapper">
           <label htmlFor="password">Password:</label>
           <input
-            type="password"
             id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            onBlur={handlePasswordError}
+            name="password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
-          {passwordError && <span className="error">{passwordError}</span>}
+          {formik.touched.password && formik.errors.password && (
+            <span className="error">{formik.errors.password}</span>
+          )}
         </div>
         <button type="submit">Login</button>
       </form>
--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,60 +1,75 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router";
 import { signUp } from "../../services/AuthService";
 import { toast } from "react-toastify";
 import "./Auth.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function Signup() {
   const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
 
-  const signup = async (event) => {
-    event.preventDefault();
-    signUp({ name, email, password })
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+  });
+
+  const onSubmit = (values, { setSubmitting }) => {
+    signUp({
+      name: values.name,
+      email: values.email,
+      password: values.password,
+    })
       .then((res) => {
         localStorage.setItem("api_key", res.data.user.api_key);
         navigate("/images");
       })
       .catch((err) => {
-        toast.error(err.message);
+        toast.error(err.response.data.message);
+      })
+      .finally(() => {
+        setSubmitting(false);
       });
   };
 
   return (
     <div className="form-page-wrapper">
-      <form className="auth-form-wrapper" onSubmit={signup}>
-        <h2>Sign Up</h2>
-        <div className="form-group-wrapper">
-          <label htmlFor="name">Name:</label>
-          <input
-            id="name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-        <div className="form-group-wrapper">
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="form-group-wrapper">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-        {error && <div className="error">{error}</div>}
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className="auth-form-wrapper">
+            <h2>Sign Up</h2>
+            <div className="form-group-wrapper">
+              <label htmlFor="name">Name:</label>
+              <Field id="name" name="name" type="text" />
+              <ErrorMessage name="name" component="div" className="error" />
+            </div>
+            <div className="form-group-wrapper">
+              <label htmlFor="email">Email:</label>
+              <Field id="email" name="email" type="email" />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
+            <div className="form-group-wrapper">
+              <label htmlFor="password">Password:</label>
+              <Field id="password" name="password" type="password" />
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Signing up..." : "Sign Up"}
+            </button>
+          </Form>
+        )}
+      </Formik>
       <div>
         Already have an account? <a href="/login">Login</a>
       </div>
